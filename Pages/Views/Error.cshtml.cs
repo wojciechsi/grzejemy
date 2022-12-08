3d7a69e57ca2e5e3b1f624c04d057cbabab7f3b6
= <=>
2022-12-08 22:03:25 +0100

    Add placeholders for MVVM in Pages and repository in data

--- Pages/Views/Error.cshtml.cs
+++ Pages/Views/Error.cshtml.cs
@@ -2,7 +2,7 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Diagnostics;
 
-namespace grzejemy.Pages
+namespace grzejemy.Pages.Views
 {
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     [IgnoreAntiforgeryToken]
