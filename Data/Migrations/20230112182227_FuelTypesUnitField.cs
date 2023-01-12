2e2b4f084d3e41e0bbd3ea97a938793dfd440767
Wojciech Siudy <wojcsiu541@student.polsl.pl>
2023-01-12 19:44:33 +0100

    Add Unit field for FuleType
    Dzianis Bova nigdy tego nie przeczyta.

---
+++ Data/Migrations/20230112182227_FuelTypesUnitField.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace grzejemy.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class FuelTypesUnitField : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Unit",
+                table: "FuelTypes",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Unit",
+                table: "FuelTypes");
+        }
+    }
+}
--- Models/FuelType.cs
+++ Models/FuelType.cs
@@ -9,8 +9,10 @@ namespace grzejemy.Models
         public FuelType()
         {
             Name = string.Empty;
+            Unit = string.Empty;
         }
         public int Id { get; set; }
         public string Name { get; set; }
+        public string Unit { get; set; }
     }
 }
